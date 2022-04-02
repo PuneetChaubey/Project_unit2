1a8f6dffa6572fe9743ccea4b0d8f885f03a8321
manojbarkicodee <manojjunjappabarki@gmail.com>
2022-04-02 21:48:26 +0530

    commit

--- mens.js
+++ mens.js
@@ -1,6 +1,16 @@
-var m=document.querySelectorAll("#menspage")
+var m=document.querySelectorAll("button")
+for(i=0;i<m.length;i++){
+    var a=m[i]
+    m[i].addEventListener("click",function(){
+        window.location.href="mens1.html"
+    })
+}
+var x=document.querySelectorAll(".tomen")
 
-document.querySelector("button").addEventListener("click",my)
-function my(){
-    console.log(m)
+for(j=0;j<x.length;j++){
+    x[j].addEventListener("click",my)
+    function my(){
+        console.log(x[j])
+        window.location.href="mens1.html"  
+    }
 }
